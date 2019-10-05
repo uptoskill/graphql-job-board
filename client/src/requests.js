8daf68e2ebca3b6739d9cb84fab5b0ce50d2fe9a
Mirko Nasato <mirko@uptoskill.com>
2019-10-05 18:21:51 +0100

    Extract generic GraphQL request function

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,45 +1,42 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
-export async function loadJob(id) {
+async function graphqlRequest(query, variables={}) {
   const response = await fetch(endpointURL, {
     method: 'POST',
     headers: {'content-type': 'application/json'},
-    body: JSON.stringify({
-      query: `query JobQuery($id: ID!) {
-        job(id: $id) {
-          id
-          title
-          company {
-            id
-            name
-          }
-          description
-        }
-      }`,
-      variables: {id}
-    })
+    body: JSON.stringify({query, variables})
   });
   const responseBody = await response.json();
-  return responseBody.data.job;
+  return responseBody.data;
+}
+
+export async function loadJob(id) {
+  const query = `query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      company {
+        id
+        name
+      }
+      description
+    }
+  }`;
+  const {job} = await graphqlRequest(query, {id});
+  return job;
 }
 
 export async function loadJobs() {
-  const response = await fetch(endpointURL, {
-    method: 'POST',
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify({
-      query: `{
-        jobs {
-          id
-          title
-          company {
-            id
-            name
-          }
-        }
-      }`
-    })
-  });
-  const responseBody = await response.json();
-  return responseBody.data.jobs;
+  const query = `{
+    jobs {
+      id
+      title
+      company {
+        id
+        name
+      }
+    }
+  }`;
+  const {jobs} = await graphqlRequest(query);
+  return jobs;
 }
