bba455bee270f46b2ad1f97dea285154da4cf58f
Mirko Nasato <mirko@uptoskill.com>
2019-10-05 18:49:30 +0100

    Call the mutation to add a new job

--- client/src/requests.js
+++ client/src/requests.js
@@ -14,6 +14,21 @@ async function graphqlRequest(query, variables={}) {
   return responseBody.data;
 }
 
+export async function createJob(input) {
+  const mutation = `mutation CreateJob($input: CreateJobInput) {
+    job: createJob(input: $input) {
+      id
+      title
+      company {
+        id
+        name
+      }
+    }
+  }`;
+  const {job} = await graphqlRequest(mutation, {input});
+  return job;
+}
+
 export async function loadCompany(id) {
   const query = `query CompanyQuery($id: ID!) {
     company(id: $id) {
