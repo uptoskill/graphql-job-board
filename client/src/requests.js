b63d02fbbb442ef808747a636b6c09e992abdd9d
Mirko Nasato <mirko@uptoskill.com>
2019-10-05 18:04:02 +0100

    Load job by id

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,5 +1,28 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
+export async function loadJob(id) {
+  const response = await fetch(endpointURL, {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      query: `query JobQuery($id: ID!) {
+        job(id: $id) {
+          id
+          title
+          company {
+            id
+            name
+          }
+          description
+        }
+      }`,
+      variables: {id}
+    })
+  });
+  const responseBody = await response.json();
+  return responseBody.data.job;
+}
+
 export async function loadJobs() {
   const response = await fetch(endpointURL, {
     method: 'POST',
