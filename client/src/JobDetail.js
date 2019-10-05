7582f1cfe18d6cfeefea6682990a41f2fb289d65
Mirko Nasato <mirko@uptoskill.com>
2019-10-05 18:04:02 +0100

    Load job by id

--- client/src/JobDetail.js
+++ client/src/JobDetail.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { jobs } from './fake-data';
+import { loadJob } from './requests';
 
 export class JobDetail extends Component {
   constructor(props) {
     super(props);
+    this.state = {job: null};
+  }
+
+  async componentDidMount() {
     const {jobId} = this.props.match.params;
-    this.state = {job: jobs.find((job) => job.id === jobId)};
+    const job = await loadJob(jobId);
+    this.setState({job});
   }
 
   render() {
     const {job} = this.state;
+    if (!job) {
+      return null;
+    }
     return (
       <div>
         <h1 className="title">{job.title}</h1>
