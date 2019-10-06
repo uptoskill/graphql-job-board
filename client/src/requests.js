d17920934503bb65b084fd1d5e34190f8d66e421
Mirko Nasato <mirko@uptoskill.com>
2019-10-06 13:00:02 +0100

    Set up Apollo Client

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,6 +1,12 @@
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import { getAccessToken, isLoggedIn } from './auth';
 
 const endpointURL = 'http://localhost:9000/graphql';
+
+const client = new ApolloClient({
+  link: new HttpLink({uri: endpointURL}),
+  cache: new InMemoryCache()
+});
 
 async function graphqlRequest(query, variables={}) {
   const request = {
