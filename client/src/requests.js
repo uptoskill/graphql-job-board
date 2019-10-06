a0e337af69b9bbe0f847d651d300583123fd27b4
Mirko Nasato <mirko@uptoskill.com>
2019-10-06 11:10:31 +0100

    Send authorization header in requests

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,11 +1,17 @@
+import { getAccessToken, isLoggedIn } from './auth';
+
 const endpointURL = 'http://localhost:9000/graphql';
 
 async function graphqlRequest(query, variables={}) {
-  const response = await fetch(endpointURL, {
+  const request = {
     method: 'POST',
     headers: {'content-type': 'application/json'},
     body: JSON.stringify({query, variables})
-  });
+  };
+  if (isLoggedIn()) {
+    request.headers['authorization'] = 'Bearer ' + getAccessToken();
+  }
+  const response = await fetch(endpointURL, request);
   const responseBody = await response.json();
   if (responseBody.errors) {
     const message = responseBody.errors.map((error) => error.message).join('\n');
