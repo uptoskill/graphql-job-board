2bbd62c4101acd90db37cc45e405b63a41fc26c0
Mirko Nasato <mirko@uptoskill.com>
2019-10-06 11:05:46 +0100

    Require user to be authenticated for createJob

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ app.use(cors(), bodyParser.json(), expressJwt({
 
 const typeDefs = gql(fs.readFileSync('./schema.graphql', {encoding: 'utf8'}));
 const resolvers = require('./resolvers');
-const apolloServer = new ApolloServer({typeDefs, resolvers});
+const context = ({req}) => ({user: req.user});
+const apolloServer = new ApolloServer({typeDefs, resolvers, context});
 apolloServer.applyMiddleware({app, path: '/graphql'});
 
 app.post('/login', (req, res) => {
