384436e9e3633050c73d32d63166c9b4856db98c
RDOToole89 <roibinotoole@gmail.com>
2020-12-28 17:22:34 +0100

    Refactor request functions, add error basic error messaging

--- client/src/JobDetail.js
+++ client/src/JobDetail.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { jobs } from './fake-data';
+import { loadJob } from './requests';
 
 export class JobDetail extends Component {
   constructor(props) {
     super(props);
-    const {jobId} = this.props.match.params;
-    this.state = {job: jobs.find((job) => job.id === jobId)};
+    this.state = { job: null };
+  }
+
+  async componentDidMount() {
+    const { jobId } = this.props.match.params;
+    const job = await loadJob(jobId);
+    this.setState({ job });
   }
 
   render() {
-    const {job} = this.state;
+    const { job } = this.state;
+    if (!job) {
+      return null;
+    }
     return (
       <div>
-        <h1 className="title">{job.title}</h1>
-        <h2 className="subtitle">
+        <h1 className='title'>{job.title}</h1>
+        <h2 className='subtitle'>
           <Link to={`/companies/${job.company.id}`}>{job.company.name}</Link>
         </h2>
-        <div className="box">{job.description}</div>
+        <div className='box'>{job.description}</div>
       </div>
     );
   }
--- client/src/requests.js
+++ client/src/requests.js
@@ -1,24 +1,99 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
-export async function loadJobs() {
+async function graphqlRequest(query, variables = {}) {
   const response = await fetch(endpointURL, {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({
-      query: `{
-        jobs {
-          id
-          title
-          company {
-            id
-            name
-          }
-        }
-      }`,
+      query,
+      variables,
     }),
   });
 
   const responseBody = await response.json();
-  console.log('BODY', responseBody.data);
-  return responseBody.data.jobs;
+  if (responseBody.errors) {
+    const message = responseBody.errors.map((error) => error.message).join('\n');
+    throw new Error(message);
+  }
+  return responseBody.data;
 }
+
+export async function loadJobs() {
+  const query = `{
+    jobs {
+      id
+      title
+      company {
+        id
+        name
+      }
+    }
+  }`;
+
+  const { jobs } = await graphqlRequest(query, {});
+
+  return jobs;
+}
+
+export async function loadJob(id) {
+  const query = `query JobQuery($id: ID!){
+    job(id:$id) {
+      id
+      title
+      company {
+        id
+        name
+      }
+      description
+    }
+}`;
+  const { job } = await graphqlRequest(query, { id });
+  return job;
+}
+
+// export async function loadJob(id) {
+//   const response = await fetch(endpointURL, {
+//     method: 'POST',
+//     headers: { 'content-type': 'application/json' },
+//     body: JSON.stringify({
+//       query: `query JobQuery($id: ID!){
+//                 job(id:$id) {
+//                   id
+//                   title
+//                   company {
+//                     id
+//                     name
+//                   }
+//                   description
+//                 }
+//             }`,
+//       variables: { id },
+//     }),
+//   });
+
+//   const responseBody = await response.json();
+//   return responseBody.data.job;
+// }
+
+// export async function loadJobs() {
+//   const response = await fetch(endpointURL, {
+//     method: 'POST',
+//     headers: { 'content-type': 'application/json' },
+//     body: JSON.stringify({
+//       query: `{
+//         jobs {
+//           id
+//           title
+//           company {
+//             id
+//             name
+//           }
+//         }
+//       }`,
+//     }),
+//   });
+
+//   const responseBody = await response.json();
+//   console.log('BODY', responseBody.data);
+//   return responseBody.data.jobs;
+// }
