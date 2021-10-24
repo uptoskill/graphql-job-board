7644b3bd78c0812e8155ee1f60083113c7904607
kushthakker <kushthakker777@gmail.com>
2021-10-24 15:13:14 +0530

    updated to apollo 3

--- server/server.js
+++ server/server.js
@@ -1,28 +1,46 @@
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const express = require('express');
-const expressJwt = require('express-jwt');
-const jwt = require('jsonwebtoken');
-const db = require('./db');
+const fs = require("fs");
+const { ApolloServer, gql } = require("apollo-server-express");
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const expressJwt = require("express-jwt");
+const jwt = require("jsonwebtoken");
+const db = require("./db");
 
 const port = 9000;
-const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
+const jwtSecret = Buffer.from("Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt", "base64");
 
-const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
+const typeDefs = gql(fs.readFileSync("./schema.graphql", { encoding: "utf8" }));
+const resolvers = require("./resolvers");
 
-app.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const user = db.users.list().find((user) => user.email === email);
-  if (!(user && user.password === password)) {
-    res.sendStatus(401);
-    return;
-  }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const app = express();
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
 
-app.listen(port, () => console.info(`Server started on port ${port}`));
+  app.use(
+    cors(),
+    express.json(),
+    expressJwt({
+      secret: jwtSecret,
+      credentialsRequired: false,
+    })
+  );
+
+  app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const user = db.users.list().find((user) => user.email === email);
+    if (!(user && user.password === password)) {
+      res.sendStatus(401);
+      return;
+    }
+    const token = jwt.sign({ sub: user.id }, jwtSecret);
+    res.send({ token });
+  });
+
+  app.listen(port, () => console.info(`Server started on port ${port}`));
+}
+
+startApolloServer(typeDefs, resolvers);
